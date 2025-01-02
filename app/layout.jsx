ebea712e62566a873f0cb3a19f4bcecbf6aad28f
pimanzi <p.imanzi@alustudent.com>
2025-01-02 21:45:05 +0200

    setting fonts configurations

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import Logo from '@/app/_components/Logo';
 import Navigation from '@/app/_components/Navigation';
 import '@//app/_styles/globals.css';
+import { Josefin_Sans } from 'next/font/google';
 export const metadata = {
   // title: 'Wild Oasis',
   title: {
@@ -10,11 +11,17 @@ export const metadata = {
   description:
     'Luxurious cabin hotel,located in Rwanda,surrounded by beautiful mountains and dark forests',
 };
+const josefin = Josefin_Sans({
+  display: 'swap',
+  subsets: ['latin'],
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="min-h-screen bg-primary-950 text-primary-100">
+      <body
+        className={`${josefin.className} min-h-screen bg-primary-950 text-primary-100`}
+      >
         <header>
           <Logo></Logo>
           <Navigation></Navigation>
