7bd19bba0c2b1d3affee1a98c962c35437d8d3e5
pimanzi <p.imanzi@alustudent.com>
2025-01-03 11:40:12 +0200

    cabins page and about page

--- starter/pages/cabins-page.js
+++ starter/pages/cabins-page.js
@@ -1,4 +1,4 @@
-import CabinCard from "@/app/_components/CabinCard";
+import CabinCard from '@/app/_components/CabinCard';
 
 export default function Page() {
   // CHANGE
@@ -13,9 +13,9 @@ export default function Page() {
         Cozy yet luxurious cabins, located right in the heart of the Italian
         Dolomites. Imagine waking up to beautiful mountain views, spending your
         days exploring the dark forests around, or just relaxing in your private
-        hot tub under the stars. Enjoy nature's beauty in your own little home
-        away from home. The perfect spot for a peaceful, calm vacation. Welcome
-        to paradise.
+        hot tub under the stars. Enjoy nature&apos;s beauty in your own little
+        home away from home. The perfect spot for a peaceful, calm vacation.
+        Welcome to paradise.
       </p>
 
       {cabins.length > 0 && (
