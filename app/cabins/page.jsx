0925539400f5d73c1419ab15e25ebb446d33cfad
pimanzi <p.imanzi@alustudent.com>
2025-01-04 21:34:11 +0200

    streamed the fetching component only using suspense

--- app/cabins/page.jsx
+++ app/cabins/page.jsx
@@ -1,12 +1,11 @@
-import CabinCard from '@/app/_components/CabinCard';
-import { getCabins } from '../_lib/data-service';
+import { Suspense } from 'react';
+import CabinsList from '../_components/CabinsList';
+import Spinner from '../_components/Spinner';
 
 export const metadata = {
   title: 'Cabins',
 };
 export default async function Page() {
-  const cabins = await getCabins();
-
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -21,13 +20,9 @@ export default async function Page() {
         Welcome to paradise.
       </p>
 
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Spinner></Spinner>}>
+        <CabinsList></CabinsList>
+      </Suspense>
     </div>
   );
 }
