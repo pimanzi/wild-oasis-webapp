cea959c994f787365f74e3f36bd78cf2ca7f348a
pimanzi <p.imanzi@alustudent.com>
2025-01-10 09:05:54 +0200

    signout logic

--- app/_lib/action.js
+++ app/_lib/action.js
@@ -1,7 +1,9 @@
 'use server';
-import { signIn } from '../_components/auth';
+import { signIn, signOut } from '../_components/auth';
 export async function signInAction() {
   await signIn('google', { redirectTo: '/account' });
 }
 
-export default signInAction;
+export async function signOutAction() {
+  await signOut({ redirectTo: '/' });
+}
