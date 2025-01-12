d416397498ac957019ecb75835f6fa6a61b26e6b
pimanzi <p.imanzi@alustudent.com>
2025-01-12 19:00:22 +0200

    updating profile logic

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,19 +1,23 @@
-"use client";
+'use client';
 
-import { useState } from "react";
+import { useState } from 'react';
+import { updateGuest } from '../_lib/action';
 
-function UpdateProfileForm({ children }) {
+function UpdateProfileForm({ children, guest }) {
   const [count, setCount] = useState();
 
-  // CHANGE
-  const countryFlag = "pt.jpg";
-  const nationality = "portugal";
+  const { fullName, email, countryFlag, nationalID, nationality } = guest;
 
   return (
-    <form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+    <form
+      action={updateGuest}
+      className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col"
+    >
       <div className="space-y-2">
         <label>Full name</label>
         <input
+          name="fullName"
+          defaultValue={fullName}
           disabled
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
@@ -22,6 +26,8 @@ function UpdateProfileForm({ children }) {
       <div className="space-y-2">
         <label>Email address</label>
         <input
+          name="email"
+          defaultValue={email}
           disabled
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
@@ -43,6 +49,7 @@ function UpdateProfileForm({ children }) {
       <div className="space-y-2">
         <label htmlFor="nationalID">National ID number</label>
         <input
+          defaultValue={nationalID}
           name="nationalID"
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
         />
--- app/_lib/action.js
+++ app/_lib/action.js
@@ -1,9 +1,33 @@
 'use server';
-import { signIn, signOut } from '../_components/auth';
+import { format } from 'date-fns';
+import { auth, signIn, signOut } from '../_components/auth';
+import { supabase } from './supabase';
 export async function signInAction() {
   await signIn('google', { redirectTo: '/account' });
 }
 
 export async function signOutAction() {
   await signOut({ redirectTo: '/' });
+}
+
+export async function updateGuest(formData) {
+  const session = await auth();
+  const nationalID = formData.get('nationalID');
+  const [nationality, countryFlag] = formData.get('nationality').split('%');
+
+  if (!/^[a-zA-Z0-9]{6,20}$/.test(nationalID))
+    throw new Error('Please provide a valid national ID');
+
+  const updateData = {
+    nationality,
+    countryFlag,
+    nationalID,
+  };
+  const { data, error } = await supabase
+    .from('guests')
+    .update(updateData)
+    .eq('id', session.user.guestId);
+  if (error) {
+    throw new Error('Guest could not be updated');
+  }
 }
