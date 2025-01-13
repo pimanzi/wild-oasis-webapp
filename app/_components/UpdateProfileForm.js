e73648355155bc7f3acc58d24034ecb6e5bf3595
pimanzi <p.imanzi@alustudent.com>
2025-01-13 20:21:08 +0200

    using useTransition

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -67,7 +67,7 @@ function Button() {
   const { pending } = useFormStatus();
   return (
     <button
-      disabled={pending}
+      disabled={pendings}
       className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
     >
       {pending ? 'Updating' : 'Update Profile'}
